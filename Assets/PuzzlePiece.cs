8c2cf5aba760eb28e00300bb46f9b9af0f4f702c
Vincent Lamicela <vtl9301@rit.edu>
2018-02-28 17:22:16 -0500

    Planning for the puzzle piece code.

---
+++ Assets/PuzzlePiece.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzlePiece : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//generate piece from color map?
+        //store every segment of the piece in an array
+            //each segment has a fill property indicating if it's inside the outline
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//click and drag
+        //when held, change color slightly and make a sound
+        //when released, make another sound and snap to nearest grid point
+        //
+	}
+}
