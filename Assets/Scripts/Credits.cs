c2952607dae17040037c60358edf4f0de60a34b0
Sean Mack <sjm3758@rit.edu>
2018-03-19 09:36:46 -0400

    Fixed main menu. added credits script

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Credits : MonoBehaviour {
+
+    public Button btn;
+
+	// Use this for initialization
+	void Start () {
+        btn = this.gameObject.GetComponent<Button>();
+        btn.onClick.AddListener(SwitchToCredits);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SwitchToCredits()
+    {
+
+    }
+}
