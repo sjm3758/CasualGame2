2fa186e44f72504c61320f209bd8d5ebc45b311a
Sean Mack <sjm3758@rit.edu>
2018-03-26 10:04:08 -0400

    added quit script

---
+++ Assets/Scripts/QuitScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class QuitScript : MonoBehaviour {
+
+    private Button btn;
+
+	// Use this for initialization
+	void Start () {
+        btn = this.gameObject.GetComponent<Button>();
+        btn.onClick.AddListener(Quit);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Quit()
+    {
+        Application.Quit();
+    }
+}
