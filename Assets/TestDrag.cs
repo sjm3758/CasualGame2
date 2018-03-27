0967986e914dc4e0cd10e2c50a3aef87e4cda7ba
PlatFleece <aaw9185@g.rit.edu>
2018-03-27 17:23:25 -0400

    Added coordinate detection system

--- Assets/TestDrag.cs
+++ Assets/TestDrag.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class TestDrag : MonoBehaviour {
 
     bool isPicked;
+    Vector3 pos;
 	
     // Use this for initialization
 	void Start () {
-		
-	}
+
+        pos = transform.position;
+       
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        pos.x = Mathf.RoundToInt(gameObject.transform.position.x);
+        pos.y = Mathf.RoundToInt(gameObject.transform.position.y);
+
         if (Input.GetMouseButtonUp(0))
         {
             isPicked = false;
+            Debug.Log(pos.x);
+            Debug.Log(pos.y);
         }
         if (isPicked == true)
         {
