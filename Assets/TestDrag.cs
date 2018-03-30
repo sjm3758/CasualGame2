762658a157e1cff41e08a3f00f3b0d15162be9d0
Plat <maverick.alpha4@gmail.com>
2018-03-30 15:04:10 -0400

    Snapping Fully Implemented

--- Assets/TestDrag.cs
+++ Assets/TestDrag.cs
@@ -15,7 +15,7 @@ public class TestDrag : MonoBehaviour {
     // Use this for initialization
 	void Start () {
 
-        pos = transform.position;
+        pos = this.transform.position;
        
 
     }
@@ -30,60 +30,273 @@ public class TestDrag : MonoBehaviour {
             isPicked = false;
             Debug.Log(pos.x);
             Debug.Log(pos.y);
-            if (pos.x < 1f && pos.x > 0.1f)
+            if (pos.x <= 1.3f && pos.x >= 0.0f)
             {
-                if (pos.y > -1f && pos.y < -0.1f)
+                
+                if (pos.y >= -1.3f && pos.y <= 0.0f)
                 {
-                    transform.position = new Vector2(0.5f, -0.5f);
-                    Debug.Log("Activated");
+                    transform.position = new Vector2(0.7f, -0.7f);
+                    
                 }
-                if (pos.y > -2f && pos.y < -0.6f)
+                if (pos.y >= -2.5f && pos.y <= -1.3f)
                 {
-                    transform.position = new Vector2(0.5f, -1.5f);
-                    Debug.Log("Activated");
+                    transform.position = new Vector2(0.7f, -2.0f);
+                    
                 }
-                if (pos.y > -3f && pos.y < -1.6f)
+                if (pos.y >= -3.8f && pos.y <= -2.5f)
                 {
-                    transform.position = new Vector2(0.5f, -2.5f);
-                    Debug.Log("Activated");
+                    transform.position = new Vector2(0.7f, -3.2f);
+                    
+                }
+                if (pos.y >= -5.2f && pos.y <= -3.8f)
+                {
+                    transform.position = new Vector2(0.7f, -4.5f);
+
+                }
+                if (pos.y >= -6.5f && pos.y <= -5.2f)
+                {
+                    transform.position = new Vector2(0.7f, -5.8f);
+
+                }
+                if (pos.y >= -7.7f && pos.y <= -6.5f)
+                {
+                    transform.position = new Vector2(0.7f, -7.1f);
+
+                }
+                if (pos.y >= -8.9f && pos.y <= -7.7f)
+                {
+                    transform.position = new Vector2(0.7f, -8.4f);
+
                 }
 
             }
-            if (pos.x < 2f && pos.x > 0.6f)
+            if (pos.x <= 2.7f && pos.x >= 1.3f)
             {
-                if (pos.y > -2f && pos.y < -0.6f)
-                {
-                    transform.position = new Vector2(1.5f, -1.5f);
-                    Debug.Log("Activated");
-                }
-                if (pos.y > -1f && pos.y < -0.1f)
-                {
-                    transform.position = new Vector2(1.5f, -0.5f);
-                    Debug.Log("Activated");
-                }
-                if (pos.y > -3f && pos.y < -1.6f)
-                {
-                    transform.position = new Vector2(1.5f, -2.5f);
-                    Debug.Log("Activated");
-                }
 
+                if (pos.y >= -1.3f && pos.y <= 0.0f)
+                {
+                    transform.position = new Vector2(2.0f, -0.7f);
+
+                }
+                if (pos.y >= -2.5f && pos.y <= -1.3f)
+                {
+                    transform.position = new Vector2(2.0f, -2.0f);
+
+                }
+                if (pos.y >= -3.8f && pos.y <= -2.5f)
+                {
+                    transform.position = new Vector2(2.0f, -3.2f);
+
+                }
+                if (pos.y >= -5.2f && pos.y <= -3.8f)
+                {
+                    transform.position = new Vector2(2.0f, -4.5f);
+
+                }
+                if (pos.y >= -6.5f && pos.y <= -5.2f)
+                {
+                    transform.position = new Vector2(2.0f, -5.8f);
+
+                }
+                if (pos.y >= -7.7f && pos.y <= -6.5f)
+                {
+                    transform.position = new Vector2(2.0f, -7.1f);
+
+                }
+                if (pos.y >= -8.9f && pos.y <= -7.7f)
+                {
+                    transform.position = new Vector2(2.0f, -8.4f);
+
+                }
             }
-            if (pos.x < 3f && pos.x > 1.6f)
+            if (pos.x < 3.9f && pos.x > 2.7f)
             {
-                if (pos.y > -2f && pos.y < -0.6f)
+                if (pos.y >= -1.3f && pos.y <= 0.0f)
                 {
-                    transform.position = new Vector2(2.5f, -1.5f);
-                    Debug.Log("Activated");
+                    transform.position = new Vector2(3.3f, -0.7f);
+
                 }
-                if (pos.y > -1f && pos.y < -0.1f)
+                if (pos.y >= -2.5f && pos.y <= -1.3f)
                 {
-                    transform.position = new Vector2(2.5f, -0.5f);
-                    Debug.Log("Activated");
+                    transform.position = new Vector2(3.3f, -2.0f);
+
                 }
-                if (pos.y > -3f && pos.y < -1.6f)
+                if (pos.y >= -3.8f && pos.y <= -2.5f)
                 {
-                    transform.position = new Vector2(2.5f, -2.5f);
-                    Debug.Log("Activated");
+                    transform.position = new Vector2(3.3f, -3.2f);
+
+                }
+                if (pos.y >= -5.2f && pos.y <= -3.8f)
+                {
+                    transform.position = new Vector2(3.3f, -4.5f);
+
+                }
+                if (pos.y >= -6.5f && pos.y <= -5.2f)
+                {
+                    transform.position = new Vector2(3.3f, -5.8f);
+
+                }
+                if (pos.y >= -7.7f && pos.y <= -6.5f)
+                {
+                    transform.position = new Vector2(3.3f, -7.1f);
+
+                }
+                if (pos.y >= -8.9f && pos.y <= -7.7f)
+                {
+                    transform.position = new Vector2(3.3f, -8.4f);
+
+                }
+            }
+            if (pos.x < 5.1f && pos.x > 3.9f)
+            {
+                if (pos.y >= -1.3f && pos.y <= 0.0f)
+                {
+                    transform.position = new Vector2(4.5f, -0.7f);
+
+                }
+                if (pos.y >= -2.5f && pos.y <= -1.3f)
+                {
+                    transform.position = new Vector2(4.5f, -2.0f);
+
+                }
+                if (pos.y >= -3.8f && pos.y <= -2.5f)
+                {
+                    transform.position = new Vector2(4.5f, -3.2f);
+
+                }
+                if (pos.y >= -5.2f && pos.y <= -3.8f)
+                {
+                    transform.position = new Vector2(4.5f, -4.5f);
+
+                }
+                if (pos.y >= -6.5f && pos.y <= -5.2f)
+                {
+                    transform.position = new Vector2(4.5f, -5.8f);
+
+                }
+                if (pos.y >= -7.7f && pos.y <= -6.5f)
+                {
+                    transform.position = new Vector2(4.5f, -7.1f);
+
+                }
+                if (pos.y >= -8.9f && pos.y <= -7.7f)
+                {
+                    transform.position = new Vector2(4.5f, -8.4f);
+
+                }
+            }
+            if (pos.x < 6.5f && pos.x > 5.1f)
+            {
+                if (pos.y >= -1.3f && pos.y <= 0.0f)
+                {
+                    transform.position = new Vector2(5.8f, -0.7f);
+
+                }
+                if (pos.y >= -2.5f && pos.y <= -1.3f)
+                {
+                    transform.position = new Vector2(5.8f, -2.0f);
+
+                }
+                if (pos.y >= -3.8f && pos.y <= -2.5f)
+                {
+                    transform.position = new Vector2(5.8f, -3.2f);
+
+                }
+                if (pos.y >= -5.2f && pos.y <= -3.8f)
+                {
+                    transform.position = new Vector2(5.8f, -4.5f);
+
+                }
+                if (pos.y >= -6.5f && pos.y <= -5.2f)
+                {
+                    transform.position = new Vector2(5.8f, -5.8f);
+
+                }
+                if (pos.y >= -7.7f && pos.y <= -6.5f)
+                {
+                    transform.position = new Vector2(5.8f, -7.1f);
+
+                }
+                if (pos.y >= -8.9f && pos.y <= -7.7f)
+                {
+                    transform.position = new Vector2(5.8f, -8.4f);
+
+                }
+            }
+            if (pos.x < 7.7f && pos.x > 6.5f)
+            {
+                if (pos.y >= -1.3f && pos.y <= 0.0f)
+                {
+                    transform.position = new Vector2(7.1f, -0.7f);
+
+                }
+                if (pos.y >= -2.5f && pos.y <= -1.3f)
+                {
+                    transform.position = new Vector2(7.1f, -2.0f);
+
+                }
+                if (pos.y >= -3.8f && pos.y <= -2.5f)
+                {
+                    transform.position = new Vector2(7.1f, -3.2f);
+
+                }
+                if (pos.y >= -5.2f && pos.y <= -3.8f)
+                {
+                    transform.position = new Vector2(7.1f, -4.5f);
+
+                }
+                if (pos.y >= -6.5f && pos.y <= -5.2f)
+                {
+                    transform.position = new Vector2(7.1f, -5.8f);
+
+                }
+                if (pos.y >= -7.7f && pos.y <= -6.5f)
+                {
+                    transform.position = new Vector2(7.1f, -7.1f);
+
+                }
+                if (pos.y >= -8.9f && pos.y <= -7.7f)
+                {
+                    transform.position = new Vector2(7.1f, -8.4f);
+
+                }
+            }
+            if (pos.x < 8.9f && pos.x > 7.7f)
+            {
+                if (pos.y >= -1.3f && pos.y <= 0.0f)
+                {
+                    transform.position = new Vector2(8.4f, -0.7f);
+
+                }
+                if (pos.y >= -2.5f && pos.y <= -1.3f)
+                {
+                    transform.position = new Vector2(8.4f, -2.0f);
+
+                }
+                if (pos.y >= -3.8f && pos.y <= -2.5f)
+                {
+                    transform.position = new Vector2(8.4f, -3.2f);
+
+                }
+                if (pos.y >= -5.2f && pos.y <= -3.8f)
+                {
+                    transform.position = new Vector2(8.4f, -4.5f);
+
+                }
+                if (pos.y >= -6.5f && pos.y <= -5.2f)
+                {
+                    transform.position = new Vector2(8.4f, -5.8f);
+
+                }
+                if (pos.y >= -7.7f && pos.y <= -6.5f)
+                {
+                    transform.position = new Vector2(8.4f, -7.1f);
+
+                }
+                if (pos.y >= -8.9f && pos.y <= -7.7f)
+                {
+                    transform.position = new Vector2(8.4f, -8.4f);
+
                 }
             }
         }
