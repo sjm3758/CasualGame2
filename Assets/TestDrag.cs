0afe786f19cb703700b21f7acb69ad0185d9c030
Plat <maverick.alpha4@gmail.com>
2018-03-30 08:52:15 -0400

    Added snapping

--- Assets/TestDrag.cs
+++ Assets/TestDrag.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class TestDrag : MonoBehaviour {
 
     bool isPicked;
+    public static int Row = 7;
+    public static int Col = 7;
+
+    public int[,] coord = new int[Row, Col];
+
     Vector3 pos;
 	
     // Use this for initialization
@@ -25,6 +30,62 @@ public class TestDrag : MonoBehaviour {
             isPicked = false;
             Debug.Log(pos.x);
             Debug.Log(pos.y);
+            if (pos.x < 1f && pos.x > 0.1f)
+            {
+                if (pos.y > -1f && pos.y < -0.1f)
+                {
+                    transform.position = new Vector2(0.5f, -0.5f);
+                    Debug.Log("Activated");
+                }
+                if (pos.y > -2f && pos.y < -0.6f)
+                {
+                    transform.position = new Vector2(1.5f, -1.5f);
+                    Debug.Log("Activated");
+                }
+                if (pos.y > -3f && pos.y < -1.6f)
+                {
+                    transform.position = new Vector2(1.5f, -2.5f);
+                    Debug.Log("Activated");
+                }
+
+            }
+            if (pos.x < 2f && pos.x > 0.6f)
+            {
+                if (pos.y > -2f && pos.y < -0.6f)
+                {
+                    transform.position = new Vector2(1.5f, -1.5f);
+                    Debug.Log("Activated");
+                }
+                if (pos.y > -1f && pos.y < -0.1f)
+                {
+                    transform.position = new Vector2(1.5f, -0.5f);
+                    Debug.Log("Activated");
+                }
+                if (pos.y > -3f && pos.y < -1.6f)
+                {
+                    transform.position = new Vector2(1.5f, -2.5f);
+                    Debug.Log("Activated");
+                }
+
+            }
+            if (pos.x < 3f && pos.x > 1.6f)
+            {
+                if (pos.y > -2f && pos.y < -0.6f)
+                {
+                    transform.position = new Vector2(1.5f, -1.5f);
+                    Debug.Log("Activated");
+                }
+                if (pos.y > -1f && pos.y < -0.1f)
+                {
+                    transform.position = new Vector2(1.5f, -0.5f);
+                    Debug.Log("Activated");
+                }
+                if (pos.y > -3f && pos.y < -1.6f)
+                {
+                    transform.position = new Vector2(1.5f, -2.5f);
+                    Debug.Log("Activated");
+                }
+            }
         }
         if (isPicked == true)
         {
