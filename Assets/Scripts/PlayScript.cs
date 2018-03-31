d19a61689a0c77b6c3e4dcf4d8290b9b64dc0cb8
Sean Mack <sjm3758@rit.edu>
2018-03-30 23:10:45 -0400

    Play button on main menu now loads level one. added reset and main menu buttons to level one

---
+++ Assets/Scripts/PlayScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayScript : MonoBehaviour {
+
+    private Button btn;
+
+	// Use this for initialization
+	void Start () {
+        btn = this.gameObject.GetComponent<Button>();
+        btn.onClick.AddListener(LoadLevel);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LoadLevel()
+    {
+        SceneManager.LoadScene("Level1test");
+    }
+}
